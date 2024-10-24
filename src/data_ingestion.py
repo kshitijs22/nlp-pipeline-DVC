909582a3ea6229ca919efcb3bfa3579cd900b42e
Kshitij <ksinfy22@gmail.com>
2024-10-24 16:39:04 +0530

    Modification due to params.yaml

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+import yaml
+
 import os
 
+test_size=yaml.safe_load(open('params.yaml','r'))['data_ingestion']['test_size']
 df=pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
 
 df.drop(columns=['tweet_id'],inplace=True)
@@ -11,7 +14,7 @@ final_df=df[df['sentiment'].isin(['happiness','sadness'])]
 
 final_df['sentiment'].replace({'happiness':1,'sadness':0},inplace=True)
 
-train_data,test_data=train_test_split(final_df,test_size=0.20,random_state=42)
+train_data,test_data=train_test_split(final_df,test_size=test_size,random_state=42)
 
 data_path=os.path.join('data','raw')
 os.makedirs(data_path)
--- src/model_building.py
+++ src/model_building.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import os
 import pickle
+import yaml
 
 from sklearn.ensemble import GradientBoostingClassifier
+
+n_estimators=yaml.safe_load(open('params.yaml','r'))['model_building']['n_estimators']
+lr=yaml.safe_load(open('params.yaml','r'))['model_building']['learning_rate']
 
 #fetch
 train_data=pd.read_csv('./data/features/train_bow.csv')
@@ -11,7 +15,7 @@ test_data=pd.read_csv('./data/features/test_bow.csv')
 X_train=train_data.iloc[:,0:-1].values
 y_train=train_data.iloc[:,-1].values
 
-clf=GradientBoostingClassifier(n_estimators=50)
+clf=GradientBoostingClassifier(n_estimators=50, learning_rate=lr)
 clf.fit(X_train,y_train)
 
 pickle.dump(clf,open('model.pkl','wb'))
