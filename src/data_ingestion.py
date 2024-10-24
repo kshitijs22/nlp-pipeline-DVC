52fdd626afa880670afb4b67fa7e7b9abfd82a7d
Kshitij <ksinfy22@gmail.com>
2024-10-25 00:30:57 +0530

    logging and exception block added in Data ingestion

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,24 +1,37 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
 import yaml
-
 import os
 
 #calling test_size from params.yaml
-test_size=yaml.safe_load(open('params.yaml','r'))['data_ingestion']['test_size']
-df=pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
+def load_params(params_path):
+    test_size=yaml.safe_load(open(params_path,'r'))['data_ingestion']['test_size']
+    return test_size
 
-df.drop(columns=['tweet_id'],inplace=True)
+def read_data(url):
+    df=pd.read_csv(url)
+    return df
 
-final_df=df[df['sentiment'].isin(['happiness','sadness'])]
+def process_data(df):
+    df.drop(columns=['tweet_id'],inplace=True)
+    final_df=df[df['sentiment'].isin(['happiness','sadness'])]
+    final_df['sentiment'].replace({'happiness':1,'sadness':0},inplace=True)
+    return final_df
 
-final_df['sentiment'].replace({'happiness':1,'sadness':0},inplace=True)
+def saved_data(train_data,test_data, data_path):
+    data_path = os.path.join(data_path, 'raw')
+    os.makedirs(data_path, exist_ok=True)
+    train_data.to_csv(os.path.join(data_path,'train.csv'))
+    test_data.to_csv(os.path.join(data_path,'test.csv'))
 
-train_data,test_data=train_test_split(final_df,test_size=test_size,random_state=42)
+def main():
+    test_size=load_params('params.yaml')
+    df=read_data('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
+    final_df=process_data(df)
 
-data_path=os.path.join('data','raw')
-os.makedirs(data_path)
+    train_data,test_data=train_test_split(final_df,test_size=test_size,random_state=42)
 
-train_data.to_csv(os.path.join(data_path,'train.csv'))
-test_data.to_csv(os.path.join(data_path,'test.csv'))
+    saved_data(train_data , test_data , data_path='data')
+
+if __name__ == "__main__":
+    main()
